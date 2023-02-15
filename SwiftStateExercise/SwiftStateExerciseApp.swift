d32e17dc13ec8a9d7ff217158223528c290395f1
Bananaslama277 <tracesmith555@gmail.com>
2023-02-15 08:39:02 -0600

    Initial Commit

---
+++ SwiftStateExercise/SwiftStateExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStateExerciseApp.swift
+//  SwiftStateExercise
+//
+//  Created by Trace Smith on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStateExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
