de8ce0608dfe73f7313936209af342783c9370a9
Bananaslama277 <tracesmith555@gmail.com>
2023-02-15 09:18:26 -0600

    First Bonus

--- SwiftStateExercise/ContentView.swift
+++ SwiftStateExercise/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     
     @State var numOne = 0
     @State var numTwo = 2
+    @State var letterIndex = 0
+    
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    
     var body: some View {
         VStack {
             Spacer()
@@ -39,7 +43,13 @@ struct ContentView: View {
                         
                         HStack {
                             Text("BONUS")
-
+                            Text(String(letterIndex))
+                            Text(letters[letterIndex])
+                            Button {
+                                letterIndex += 1
+                            } label : {
+                                Text("NEXT LETTER")
+                            }
                         }
                         
                         Spacer()
